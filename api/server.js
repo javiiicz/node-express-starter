2d4e7b3f43156f47fc95f678d0ad22bd858b010d
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 10:56:48 -0700

    [Server] Add endpoints

--- api/server.js
+++ api/server.js
@@ -5,14 +5,19 @@ const express = require("express");
 
 const server = express();
 
+server.get('/api/pets', (req, res, next) => {
+    res.status(200).json([
+        {id:1, name: 'Fido'},
+        {if:2, name: 'Bicho'}
+    ])
+})
+
+
 server.use("/*", (req, res, next) => {
     // req:request object
     // res: response object
     // next: a special object that signals req and res to move to next piece of pipeline
-
-    res.status(200).json('You called the server...');
-
-    
+    res.status(404).json({ message: 'Not Found' });
 });
 
 module.exports = server;
