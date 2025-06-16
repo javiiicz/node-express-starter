a0d867ddbe7f6af081b54de6d7d8c53dac621b19
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 10:56:41 -0700

    [Server] Setup server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // entry point of the program
-console.log('I love javascript.')
+console.log("I love javascript.");
+
+const server = require("./api/server");
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
