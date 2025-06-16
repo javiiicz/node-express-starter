499cc6f5e0ae7d62a0263560c91159846ab0ac70
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 10:24:48 -0700

    [Server] Catchall endpoint

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // express server
+
+// old express importing / exporting process
+const express = require("express");
+
+const server = express();
+
+server.use("/*", (req, res, next) => {
+    // req:request object
+    // res: response object
+    // next: a special object that signals req and res to move to next piece of pipeline
+
+    res.status(200).json('You called the server...');
+
+    
+});
+
+module.exports = server;
